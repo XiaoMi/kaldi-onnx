35f71733b273f06749a7744ff3b787dcd92cbadd
jasontu <jasontu@wezhuiyi.com>
2020-05-27 15:57:09 +0800

    格式调整

--- converter/graph.py
+++ converter/graph.py
@@ -1,23 +1,8 @@
-# Copyright 2019 Xiaomi, Inc.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Created by tz301 on 2020/05/26
+"""Graph build."""
 
-"""
-converter.graph - class to manage graph manipulation on top of ONNX
-"""
-
-from __future__ import division
-from __future__ import print_function
 
 from onnx import checker, defs, helper, numpy_helper, onnx_pb
 
